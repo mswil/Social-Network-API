1e4fe8611af5afef580d62a5fcb1ff7c3945f967
Molly Wilson <molly_wilson1@outlook.com>
2021-07-18 16:23:43 -0500

    User model has username, email, thoughts, friends, and friendCount

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please enter a vailid email address"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virturals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtural('friendCount').get()
+
+const User = model('User', UserSchema);
+
+module.exports = User;
